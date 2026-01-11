bbdf2e0dab7698614ec84325b9e8e09da29428e9
javier garcia alvarez <javigarcial87@gmail.com>
2026-01-11 20:05:54 +0100

    conexion con el servidor y rutas en app.js

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 const express = require('express');
 const app = express();
+
+const usuariosRoutes = require('./routes/usuarios.routes');
+const { notFound } = require('./middlewares');
+
+//middlewares globales
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+//rutas
+
+app.use('/usuarios', usuariosRoutes);
+
+
+//error 404
+
+app.use(notFound);
+
+//servidor
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando en http://localhost:3000');
+});
+
+
+
+
+
+
+
+
